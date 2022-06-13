38440f526a4da8a37043f7c8db3ac06f015e4a26
Nicholas Ng <onenylxus@gmail.com>
2022-06-14 01:23:12 +0800

    Header options

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setType, setLimit, setTheme, setTimer, setList } from '../services/actions';
+import { reset } from '../services/reset';
 import library from '../library.json';
 import '../styles/Header.scss';
 
@@ -18,6 +19,33 @@ export default function Header() {
 
   const dispatch = useDispatch();
 
+  const toggleType = () => {
+    if (type !== 0) {
+      const i = (pref.type.indexOf(type) + 1) % pref.type.length;
+      dispatch(setType(pref.type[i]));
+      localStorage.setItem('type', type);
+      reset();
+    }
+  };
+
+  const toggleLimit = () => {
+    if (limit !== 0) {
+      const i = (pref.limit.indexOf(limit) + 1) % pref.limit.length;
+      dispatch(setLimit(pref.limit[i]));
+      localStorage.setItem('limit', limit);
+      reset();
+    }
+  };
+
+  const toggleTheme = () => {
+    if (theme !== 0) {
+      const i = (pref.theme.indexOf(theme) + 1) % pref.theme.length;
+      dispatch(setTheme(pref.theme[i]));
+      localStorage.setItem('theme', theme);
+      reset();
+    }
+  };
+
   useEffect(() => {
     const type = localStorage.getItem('type') || 'words';
     const limit = +localStorage.getItem('limit') || 60;
@@ -34,6 +62,11 @@ export default function Header() {
   return (
     <header>
       <a href="." className="logo">teletype</a>
+      <div className="options">
+        <div className="type" key="type">type<button className="option" onClick={(e) => toggleType(e)}>{type}</button></div>
+        <div className="limit" key="limit">time<button className="option" onClick={(e) => toggleLimit(e)}>{limit}</button></div>
+        <div className="theme" key="theme">theme<button className="option" onClick={(e) => toggleTheme(e)}>{theme}</button></div>
+      </div>
     </header>
   );
 }
